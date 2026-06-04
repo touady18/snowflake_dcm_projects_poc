558a3744bc664fd082443d48fc35d065f77b79e5
touady18 <touahridyhia@gmail.com>
2026-06-04 10:57:17 +0200

    add test

--- play_demo/sources/definitions/test_some_cases.sql
+++ play_demo/sources/definitions/test_some_cases.sql
@@ -12,3 +12,9 @@ DEFINE TABLE  DCM_TEST_CASES.test_cases.some_cases (
     --,test_new_column VARCHAR(50)  -- new column added for testing purposes
 
 );
+
+-- Created table 2
+DEFINE TABLE  DCM_TEST_CASES.test_cases.test_fact_demo (
+    id INT,
+    name_demo VARCHAR
+);
